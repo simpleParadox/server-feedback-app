c9b248a1ee741e1080c09387a44f917eae83b211
Rohan Saha <abomicode@gmail.com>
2018-11-02 13:29:16 +0530

    Enabled GoogleOauth

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express'); // Requiring the 'express' package.
+const passport = require('passport');
+const key = require('./key.json');
+const GoogleStrategy = require('passport-google-oauth20').Strategy; // Exports the 'Strategy' properties
 const app = express(); // Creating the express application. Contains all the underlying utility functions.
 
 
 const PORT = process.env.PORT || 3000; // Use the dynamically defined port or development port.
-// 'get' call
-// '/' is the root route.
-app.get('/', (req, res) => {
-  //'req' represents the incoming request object.
-  //'res' represents the outgoing response object.
-  res.send({hi: 'world'});
-});
+
+// Make passport work with GoogleStrategy to authenticate with Google SignIn.
+passport.use(new GoogleStrategy()); // Create a new GoogleStrategy for authenticating a user.
+
 
 app.listen(PORT);
